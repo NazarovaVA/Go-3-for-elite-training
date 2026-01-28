240ce8e5b582b642613d30f7c3a99eaf21d5a804
NazarovaVA <nazarovava@internet.ru>
2026-01-29 01:42:46 +0300

    Seventh task

---
+++ 7/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	sum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, err := strconv.Atoi(line)
+		if err == nil {
+			sum += num
+		}
+	}
+	if err := scanner.Err(); err != nil && err != io.EOF {
+		fmt.Fprintln(os.Stderr, "Ошибка:", err)
+		return
+	}
+	fmt.Println(sum)
+	//Чтобы вывилась сумма, надо нажать Ctrl+Z
+}
